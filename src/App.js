bbf80aabff1f7f96578f4e488692229c1747bc68
Kyle Nakamura <kjnakamura@apu.edu>
2019-03-14 03:26:17 -0700

    Successfully query 50 businesses from Yelp Fusion.
    Create two constants which are set by hidden variables in `.env`.
    Install npm package `yelp-fusion`.
    Use `yelp-fusion` to query 50 businesses. Print the result.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+'use strict';
+
 import React, { Component } from 'react';
 import University from './components/University';
 import Header from './components/Header';
 import './App.css';
+
+// Yelp Fusion API Credentials.
+// Property of Unibui Inc.
+const API_CLIENT = process.env.REACT_APP_YELP_CLIENT_ID;
+const API_KEY = process.env.REACT_APP_YELP_API_KEY;
+
+(function fetchYelpBusinesses(businessType, maxDistance) {
+    let result = {};
+    
+    // TODO: 
+   	// (in progress) Fetch results from Yelp.
+   	
+	const yelp = require('yelp-fusion');
+
+	// Place holder for Yelp Fusion's API Key. Grab them
+	// from https://www.yelp.com/developers/v3/manage_app
+	const apiKey = '5PhACrZEOw4XW_8PBt8VxZTc7ZYkLpatvmNVJLKG60slfnAj_I3ajJfnriq2ZTM_hLSaDyFH2yIJ0by6TZqvwtCPqaRgDunoMdjFLtV4HpQtYYPWSG06nWa-JqDvW3Yx';
+
+	const searchRequest = {
+	  term: 	'Four Barrel Coffee',
+	  location: 'san francisco, ca',
+	  limit: 	'50'
+	};
+
+	const client = yelp.client(apiKey);
+
+	client.search(searchRequest).then(response => {
+	  const body = response.jsonBody;
+	  const prettyJson = JSON.stringify(body, null, 4);
+	  console.log(prettyJson);
+	}).catch(e => {
+	  console.log(e);
+	});
+
+    return result;
+})();
 
 class App extends Component {
 	// TODO: Populate state using Yelp Fusion API.
