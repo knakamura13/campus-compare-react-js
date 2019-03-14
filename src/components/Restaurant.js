11433c6dd3986947adb48e2365b8966c62b5b90f
Kyle Nakamura <kjnakamura@apu.edu>
2019-03-13 23:06:32 -0700

    Finish main setup and display a basic title page.

---
+++ src/components/Restaurant.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Restaurant extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Campus Compare</h1>
+      </div>
+    );
+  }
+}
+
+export default Restaurant;
